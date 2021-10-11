25199dcf688b33b460631a3d779671b8cbfc17d1
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-11 21:15:02 +0530

    ZEENAT: [added] main method

---
+++ Day20_MoodAnalyser/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day20_MoodAnalyser
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("****************WELCOME TO MOOD ANALYSER PROGRAMM******************");
+            }
+    }
+}
+
+
