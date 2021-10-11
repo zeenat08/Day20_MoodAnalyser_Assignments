67aa547ea6a06f72e3bb36925dc78c4f19457e68
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-11 21:14:26 +0530

    ZEENAT: [added] Mood Analyser custom Exception file

---
+++ Day20_MoodAnalyser/Mood_Analyser_Custom_Exception.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day20_MoodAnalyser
+{
+    public class Mood_Analyser_Custom_Exception : Exception
+    {
+        public enum ExceptionType
+        {
+            NULL_MESSAGE,
+            EMPTY_MESSAGE,
+            NO_SUCH_FIELD,
+            NO_SUCH_METHOD,
+            NO_SUCH_CLASS,
+            OBJECT_CREATION_ISSUE
+        }
+        private readonly ExceptionType type;
+        public Mood_Analyser_Custom_Exception(ExceptionType Type, String message) : base(message)
+        {
+            this.type = Type;
+        }
+    }
+}
+
